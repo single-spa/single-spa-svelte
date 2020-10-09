3f0569d14c6b301487f11cf5abf12b1db6d80a5d
Geoffrey Caven <geoffreycaven@gmail.com>
2020-10-09 19:48:54 -0300

    Add update lifecycle method (#10)
    * Add update lifecycle method
    
    * let -> const
    
    Co-authored-by: Geoffrey Caven <geoff.caven@nbx.com>
    Co-authored-by: Geoffrey Caven <51962809+gcaven-nbx@users.noreply.github.com>

--- src/single-spa-svelte.test.js
+++ src/single-spa-svelte.test.js
@@ -3,8 +3,16 @@ import singleSpaSvelte from "./single-spa-svelte";
 describe(`single-spa-svelte`, () => {
   it("can bootstrap, mount, and unmount a svelte application", async () => {
     const component = jest.fn();
+    let props = {
+      name: "app1",
+      foo: "bar"
+    };
+    const $set = jest.fn(
+      newProps => (props = Object.assign({}, props, newProps))
+    );
     const $destroy = jest.fn();
     component.mockImplementationOnce(function() {
+      this.$set = $set;
       this.$destroy = $destroy;
     });
     const lifecycles = singleSpaSvelte({
@@ -13,10 +21,6 @@ describe(`single-spa-svelte`, () => {
         thing: "value"
       }
     });
-    const props = {
-      name: "app1",
-      foo: "bar"
-    };
     expect(component).not.toHaveBeenCalled();
     expect($destroy).not.toHaveBeenCalled();
     await lifecycles.bootstrap(props);
@@ -33,6 +37,10 @@ describe(`single-spa-svelte`, () => {
       name: "app1"
     });
     expect($destroy).not.toHaveBeenCalled();
+    await lifecycles.update({ foo: "notbar" });
+    expect($destroy).not.toHaveBeenCalled();
+    expect($set).toHaveBeenCalled();
+    expect(props.foo).toEqual("notbar");
     await lifecycles.unmount();
     expect($destroy).toHaveBeenCalled();
   });
