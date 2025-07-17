bcd732b6ce620cbddc4b7e11d71a921339e4de5a
Maarten Manders <maarten.manders@dualinventive.com>
2025-07-17 13:28:15 +0200

    Revert babel removal

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "eslint/config";
+import babelParser from "@babel/eslint-parser";
 
 export default defineConfig([
   {
     languageOptions: {
       parserOptions: {
+        parser: babelParser,
         ecmaVersion: "latest",
       },
     },
